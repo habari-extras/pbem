983570ca43480a08e8363cc6b4cdb7b5d91e4265
mikelietz <codger+git@gmail.com>
2009-09-06 19:09:45 +0000

    plugin:pbem First stab at making this multipart-aware. It does store (some) attachments, but the text portions of the emaails isn't getting picked up. Also, no img tag is being created from the attachment(s). Storing them in the file silo, but not sure if it should also generate and store a .deriv thumbnail to play more nicely with the silo.
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/pbem/trunk@2257 f4a90942-1786-4aa1-b092-57c31935ddc3

--- pbem.plugin.php
+++ pbem.plugin.php
@@ -7,6 +7,29 @@
  */
 class pbem extends Plugin
 {
+	/**
+	 * Save attached JPG, PNG, and GIF files to user/files/pbem
+	 **/
+	public static function store_attachment( $filename = null, $content = null ) {
+
+		// Does pbem directory not exist? * Copied from Habari File Silo
+		$pbemdir = Site::get_dir( 'user' ) . '/files/PBEM';
+// Utils::debug( $pbemdir );
+		if ( !is_dir( $pbemdir ) ) {
+
+			// Create the pbem directory
+			if ( !mkdir( $pbemdir, 0755 ) ) {
+				return false;
+			}
+		}
+		$filename = "$pbemdir/$filename";
+// Utils::debug( $content );
+Utils::debug( $filename );
+			$dest = fopen( $filename, 'w+') or die( 'cannot open for writing') ;
+			fwrite( $dest, $content );
+			return fclose( $dest );
+	}
+
 	public function action_plugin_activation( $file )
 	{
 		if ( realpath( $file ) == __FILE__ ) {
@@ -46,18 +69,93 @@ class pbem extends Plugin
 			$server_password = $user->info->pbem__server_password;
 
 		if ($server_string) {
-			$mh = imap_open($server_string, $server_username, $server_password, OP_SILENT | OP_DEBUG);
-			$n = imap_num_msg($mh);
-			for ($i = 1; $i <= $n; $i++) {
-				$body = imap_body($mh, $i);
-				$header = imap_header($mh, $i);
+			$mh = imap_open( $server_string, $server_username, $server_password, OP_SILENT | OP_DEBUG )
+				or Eventlog::log( _t( 'Unable to connect' ) ); // get a better error, one with imap_*
+			$n = imap_num_msg( $mh );
+			for ( $i = 1; $i <= $n; $i++ ) {
+
+				$body = '';
+
+				$header = imap_header( $mh, $i );
+				$structure = imap_fetchstructure( $mh, $i );
+
+				if ( !isset( $structure->parts )) {
+					// message is not not multipart
+					$body = imap_body( $mh, $i ); // fetchbody only works for single part messages.
+					if ( $structure->encoding == 4 ) {
+						$body = quoted_printable_decode( $body );
+
+						// there's room here for more stuff, with strtoupper($structure->subtype...)
+					}
+// Utils::debug( 'not multipart!' );
+
+				} 
+				else {
+					$attachments = array();
+					if ( isset( $structure->parts ) && count( $structure->parts ) ) {
+
+						for($j = 0; $j < count($structure->parts); $j++) {
+
+						$attachments[$j] = array(
+							'is_attachment' => false,
+							'filename' => '',
+							'subtype' => '',
+							'name' => '',
+							'attachment' => ''
+						);
+// Utils::debug($structure->parts[$j]->subtype); 		
+
+						if ( $structure->parts[$j]->ifdparameters ) {
+							foreach( $structure->parts[$j]->dparameters as $object ) {
+								if ( strtolower( $object->attribute ) == 'filename' ) {
+									$attachments[$j]['is_attachment'] = true;
+									$attachments[$j]['filename'] = $object->value;
+									$attachments[$j]['subtype'] = $structure->parts[$j]->subtype;
+								}
+							}
+						}
+						elseif ( strtolower ($structure->parts[$j]->subtype)  == 'plain' ) {
+							$body .= imap_fetchbody( $mh, $i, $j+1 );			
+// Utils::debug( 'PLAIN!!!' );			
+						}
+		
+						if ( $structure->parts[$j]->ifparameters ) {
+							foreach( $structure->parts[$j]->parameters as $object ) {
+								if( strtolower( $object->attribute ) == 'name') {
+									$attachments[$j]['is_attachment'] = true;
+									$attachments[$j]['name'] = $object->value;
+									if ( !isset( $attachments[$j]['subtype'] ) ) { // may not be necessary
+										$attachments[$j]['subtype'] = $structure->parts[$j]->subtype; // may not be necessary
+									} // may not be necessary
+								}
+							}
+						}
+
+						if( $attachments[$j]['is_attachment'] ) {
+							$attachments[$j]['attachment'] = imap_fetchbody($mh, $i, $j+1);
+
+							if( $structure->parts[$j]->encoding == 3 ) { // 3 = BASE64
+								$attachments[$j]['attachment'] = base64_decode( $attachments[$j]['attachment'] );
+								self::store_attachment( $attachments[$j]['filename'], $attachments[$j]['attachment'] );
+							}
+							elseif ( $structure->parts[$j]->encoding == 4) { // 4 = QUOTED-PRINTABLE
+								$attachments[$j]['attachment'] = quoted_printable_decode($attachments[$j]['attachment']);
+								$body .= $attachments[$j]['attachment'];
+							}
+						}
+					}
+				}
+				}
+
 				$tags = '';
-				if (stripos($body, 'tags:') === 0) {
+
+				// if the first line of the message starts with 'tags:', read that line into tags.
+				if ( stripos($body, 'tags:' ) === 0) {
 					list($tags, $body) = explode("\n", $body, 2);
 					$tags = trim(substr($tags, 5));
 					$body = trim($body);
 				}
-
+Utils::debug( $structure);
 				$postdata = array(
 					'slug' =>$header->subject,
 					'title' => $header->subject,
@@ -68,8 +166,8 @@ class pbem extends Plugin
 					'status' => Post::status('published'),
 					'content_type' => Post::type('entry'),
 				);
-
-// Utils::debug( $postdata ); 
+Utils::debug( $postdata ); 
+Utils::debug( $attachments );
 				// This htmlspecialchars makes logs with &lt; &gt; etc. Is there a better way to sanitize?
 				EventLog::log( htmlspecialchars( sprintf( 'Mail from %1$s (%2$s): "%3$s" (%4$d bytes)', $header->fromaddress, $header->date, $header->subject, $header->Size ) ) );
 
@@ -77,7 +175,7 @@ class pbem extends Plugin
 
 				if ($post) {
 					// done with the message, now delete it. Comment out if you're testing.
-					imap_delete( $mh, $i );
+//					imap_delete( $mh, $i );
 				}
 				else {
 					EventLog::log( 'Failed to create a new post?' );
@@ -149,6 +247,43 @@ class pbem extends Plugin
 		return $require_any; 
 	}
 
-}
+	/**
+	 * Initialize some internal values when plugin initializes
+	 * Copied from Habari File Silo
+	 */
+	public function action_init()
+	{
+		$user_path = HABARI_PATH . '/' . Site::get_path('user', true);
+		$this->root = $user_path . 'files'; //Options::get('simple_file_root');
+		$this->url = Site::get_url('user', true) . 'files';  //Options::get('simple_file_url');
 
+		if ( !$this->check_files() ) {
+			Session::error( _t( "The web server does not have permission to create the 'files' directory for saving attachments (THis directory is also used for the Habari Media Silo." ) );
+			Plugins::deactivate_plugin( __FILE__ ); //Deactivate plugin
+			Utils::redirect(); //Refresh page. Unfortunately, if not done so then results don't appear
+		}
+	}
+
+	/**
+	 * Checks if files directory is usable
+	 * Copied from Habari File Silo
+	 */
+	private function check_files() {
+		$user_path = HABARI_PATH . '/' . Site::get_path('user', true);
+		$this->root = $user_path . 'files'; 
+		$this->url = Site::get_url('user', true) . 'files'; 
+
+		if ( !is_dir( $this->root ) ) {
+			if ( is_writable( $user_path ) ) {
+				mkdir( $this->root, 0755 );
+			}
+			else {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+}
 ?>
